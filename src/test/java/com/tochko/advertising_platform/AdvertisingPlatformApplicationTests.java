0e4ca0c3410a2da0415aafe2e609012776950d9e
TochkoSonya <sonya.tochko@live.com>
2021-10-04 10:34:43 +0300

    initial commit

---
+++ src/test/java/com/tochko/advertising_platform/AdvertisingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tochko.advertising_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
